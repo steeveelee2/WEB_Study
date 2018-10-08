6915af59c7f679d523c09e67dd48e77386705177
steeveelee <steeveelee2@gmail.com>
2018-10-08 09:48:23 +0900

    RBF

---
+++ JQuery/resource/js/rainbowColor.js
@@ -0,0 +1,24 @@
+$(()=>{
+    $('input:checkbox').click(function(){
+        if($(this).parent().attr('id')=='row0'){
+            var idx=$(this).index(); var color=$(this).val();
+            $(this).prop('checked')&&
+                $('td[id*="row"]').each(function(){
+                    $(this).addClass(color).children().each(function(){
+                        $(this).prop('checked',$(this).index()==idx?true:false);
+                    });
+                });
+            $(this).prop('checked')||$('#resetFrm').trigger('click');
+        }else{
+            $(this).prop('checked')&&
+                $('#row0').removeClass().children().prop('checked',false);
+                $(this).siblings().prop('checked',false);
+                $(this).parent().addClass($(this).val());
+            $(this).prop('checked')||$(this).parent().removeClass();
+        }
+    });
+});
+$('#resetFrm').click(()=>{
+    $('table *').parent().removeClass();
+    $('input:checkbox').prop('checked',false);
+});
